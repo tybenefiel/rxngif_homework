dd5e33e2a6b36151d84e16c389324f478aefe02e
Ty Benefiel <tbenefiel2014@kellogg.northwestern.edu>
2013-11-04 16:43:13 -0600

    Finished HW

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,37 @@
 class PicturesController < ApplicationController
   def show
+    @pic = Picture.find(params[:id])
   end
+
+  def index
+    @pic = Picture.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @i = Picture.new
+    @i.caption = params[:caption]
+    @i.source = params[:source]
+    @i.save
+  end
+
+  def destroy
+    i = Picture.find(params[:id])
+    i.destroy
+  end
+
+  def edit
+    @pic = Picture.find(params[:id])
+  end
+
+  def update
+    @i = Picture.find(params[:id])
+    @i.caption = params[:caption]
+    @i.source = params[:source]
+    @i.save
+  end
+
 end
